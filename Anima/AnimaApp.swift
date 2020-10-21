bd7fe0bd10d449205e5714b01681894c0a6338c8
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-21 19:08:10 +0300

    Initial Commit

---
+++ Anima/AnimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaApp.swift
+//  Anima
+//
+//  Created by Mete Alp Kizilcay on 21.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct AnimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
